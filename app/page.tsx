f128bc5c16fd2ac2c6303414b03d103acb6effe1
Gabriel Scremim <gabriel.vaz@gmaster.com.br>
2026-02-24 15:55:47 -0300

    feat: simplificar redirecionamento na página inicial e ajustar script de desenvolvimento

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('token')?.value;
-  console.log("Token no server-side:", token);
-
-  if (token) {
-    redirect('/home');
-  } else {
-    redirect('/login');
-  }
+export default function Page() {
+  redirect('/login')
 }
